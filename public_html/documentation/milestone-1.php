7966896b13f995a60dfd9f06d46865553d8d343d
Nicklas Pascetti <Npascetti@gmail.com>
2017-10-18 19:14:39 -0600

    added h1 tags, and purpose section

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,6 +5,10 @@
 		<title>Nick Pascetti's PWP</title>
 	</head>
 	<body>
-
+		<h1>Purpose</h1>
+		<p>To aid our clients in the growth of their business and brand with a unique, personalized, and customized web presence.</p>
+		<h1>Audience</h1>
+		<h1>Goal</h1>
+		<h1>Persona</h1>
 	</body>
 </html>
