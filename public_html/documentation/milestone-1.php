9e1aced890074334d9844c2ff15a69dabdd94c0c
Nicklas Pascetti <Npascetti@gmail.com>
2017-10-18 19:37:46 -0600

    completed audience and goal sections

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -8,7 +8,10 @@
 		<h1>Purpose</h1>
 		<p>To aid our clients in the growth of their business and brand with a unique, personalized, and customized web presence.</p>
 		<h1>Audience</h1>
+		<p>The target audience for this site is potential clients and employers<p>
 		<h1>Goal</h1>
+		<p>To act as a portfolio to display my work, and inform potential clients about my services.</p>
 		<h1>Persona</h1>
+
 	</body>
 </html>
