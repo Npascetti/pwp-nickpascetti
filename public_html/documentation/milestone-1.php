bf5253f41f510b2f5ae040238e30485c63e448b6
Nicklas Pascetti <Npascetti@gmail.com>
2017-10-18 21:57:49 -0600

    updated persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -18,9 +18,10 @@
 			<li>Education: MBA </li>
 			<li>Occupation: Accountant</li>
 			<li>Technology: Microsoft Surface Pro 4, custom PC Workstation</li>
-			<li>Hobbies: Fitness</li>
+			<li>Hobbies: Fitness, Investing</li>
 			<li>Relationship Status: Single</li>
 		</ul>
-		<p></p>
+		<p>Scott has been working as an accountant at a local furniture store for about 15 years. He likes to play with investment strategies, and has always been career oriented. In his spare time, and for extra income, Scott helps advise friends and family on their finances.</p>
+		<p>Scott wants to "step up his game" and take his side gig "to the next level" by branching out as a financial and accounting consultant. He is looking to create a web presence to attract clients for his new business that conveys his holistic attitude towards financial and physical health and well being.</p>
 	</body>
 </html>
