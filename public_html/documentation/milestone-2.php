427896ee43c80470ce578107ddc44da87c4cd500
Nicklas Pascetti <Npascetti@gmail.com>
2017-10-28 12:31:14 -0600

    created milestone-2.php, created html boilerplate, and h1 tags for 2a

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Nick Pascetti's PWP milestone 2a</title>
+	</head>
+	<body>
+		<h1>Content Strategy</h1>
+		<h1>Wireframes</h1>
+	</body>
+</html>
