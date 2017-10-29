b0c096d24a646edae273654e04f6fd525ac00a3e
Nicklas Pascetti <Npascetti@gmail.com>
2017-10-28 20:38:55 -0600

    added Contact section of Content Strategy

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -1,6 +1,7 @@
 <!DOCTYPE HTML>
 <html lang="en">
 	<!--Potential Fonts - Ubuntu, Raleway, Roboto Condensed, Fjalla One, Inconsolata, Asap-->
+	<!-- Color scheme will follow a special character and punctuation color palette differentiation, i.e, differnt colored periods, @ signs, etc.-->
 	<head>
 		<meta charset="utf-8" />
 		<title>Nick Pascetti's PWP milestone 2a</title>
@@ -8,14 +9,16 @@
 	<body>
 		<h1>Content Strategy</h1>
 		<h2><em>Me</em> Section</h2>
-		<p>A section with a brief description of who I am as a developer, including professional headshot.</p>
-		<p>Customized web and software solutions designed by me, tailored for you, emphasizing security, functionality and minimalist aesthetic design.</p>
+		<p>A section with a brief description of who I am as a developer.</p>
+		<p>Customized web and software solutions designed by me, tailored for you, emphasizing security, functionality and minimalist aesthetic design. Taking you where you want to go.</p>
 		<h2><em>Services</em> Section</h2>
 		<p>A section listing the services I offer.</p>
-		<p>Web design and development, UI, UX, Responsive Design, App design</p>
+		<p>Web design and development, UI, UX, Responsive Design, App design. Not ready to take the full plunge? We offer customized Wordpress and CMS solutions as well.</p>
 		<h2><em>Portfolio of Work</em> Section</h2>
 		<p>A thumbnail gallery displaying my best projects.</p>
-		<h2><em>Contact</em> Session</h2>
+		<h2><em>Contact</em> Section</h2>
+		<p>Section including contact form, email contact, links to Twitter, GitHub, LinkedIn, etc., as well as my headshot.</p>
+		<p>Let's talk. Contact form will contain a Your Name, Email Address, Phone Number, and What You Want section.</p>
 		<h1>Wireframes</h1>
 	</body>
 </html>
