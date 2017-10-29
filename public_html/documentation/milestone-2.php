74b821c3a6f88b2ec9cb4001a91d024fd6e6e851
Nicklas Pascetti <Npascetti@gmail.com>
2017-10-28 19:48:38 -0600

    added description of Me Section for content strategy, and added Services h2

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -6,6 +6,10 @@
 	</head>
 	<body>
 		<h1>Content Strategy</h1>
+		<h2><em>Me</em> Section</h2>
+		<p>Customized web solutions designed by me, tailored for you, emphasizing security, functionality and minimalist aesthetic design.</p>
+		<h2><em>Services</em> Section</h2>
+		<p></p>
 		<h1>Wireframes</h1>
 	</body>
 </html>
