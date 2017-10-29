8437cf89a3e35c1d315499af940073c240e0a1f5
Nicklas Pascetti <Npascetti@gmail.com>
2017-10-28 20:04:36 -0600

    wrote services section, added portfolio of work h2

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -7,9 +7,12 @@
 	<body>
 		<h1>Content Strategy</h1>
 		<h2><em>Me</em> Section</h2>
-		<p>Customized web solutions designed by me, tailored for you, emphasizing security, functionality and minimalist aesthetic design.</p>
+		<p>A section with a brief description of who I am as a developer, including professional headshot.</p>
+		<p>Customized web and software solutions designed by me, tailored for you, emphasizing security, functionality and minimalist aesthetic design.</p>
 		<h2><em>Services</em> Section</h2>
-		<p></p>
+		<p>A section listing the services I offer.</p>
+		<p>Web design and development, UI, UX, Responsive Design, App design</p>
+		<h2><em>Portfolio of Work</em> Section</h2>
 		<h1>Wireframes</h1>
 	</body>
 </html>
