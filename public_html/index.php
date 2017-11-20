09feea7f17207bf8458765ea125d2f515625e3da
Nicklas Pascetti <Npascetti@gmail.com>
2017-11-20 08:26:05 -0700

    set up divs, dividing up content sections

--- public_html/index.php
+++ public_html/index.php
@@ -13,111 +13,37 @@
 		<title>Nick's PWP</title>
 	</head>
 	<body>
+<!--		Div containing landing area, nav, and background image-->
+		<div container-fluid>
+
+		</div>
+<!--		Div for services section-->
+		<div container>
+
+		</div>
+<!--		Div containing services cards-->
+		<div>
+
+		</div>
+<!--		div containing footer for services section-->
+		<div>
+
+		</div>
+<!--		Div containing the portfolio section-->
 		<div class="container">
-			<nav class="navbar navbar-expand-md navbar-light bg-light mb-3 mt-3 border border-dark rounded">
-				<a class="navbar-brand" href="#">Bob Ross</a>
-				<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul class="navbar-nav ml-auto">
-						<li class="nav-item ">
-							<a class="nav-link" href="#">Clouds</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">Squirrels</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">Trees</a>
-						</li>
-					</ul>
-				</div>
-			</nav>
+
 		</div>
+<!--		Div containg header for Contact Section-->
 		<div class="container">
-			<div class="jumbotron mt-5 mb-5">
-				<h1 class="display-2">Happy, little trees!</h1>
-				<p class="lead">Let's make some happy little clouds in our world. In life you need colors. And right there you got an almighty cloud.</p>
-				<p class="lead">
-					<a class="btn btn-primary btn-lg" href="#" role="button">Tree me</a>
-				</p>
-			</div>
+
 		</div>
-		<div class="container-fluid bg-secondary p-5 mb-5 d-flex-md flex-column">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-3 d-flex align-items-center justify-content-center ">
-						<img src="http://www.fillmurray.com/250/250" alt="filler" class="img-fluid col-xs-12 d-block""/>
-					</div>
-					<div class="col-md-9 text-justify"">
-					<p class="pl-1 pt-3">It's a very cold picture, I may have to go get my coat. It's about to freeze me to death. You can spend all day playing with mountains. I started painting as a hobby when I was little. I didn't know I had any talent. I believe talent is just a pursued interest. Anybody can do what I do. You can do it. Put it in, leave it alone. And just raise cain. If you didn't have baby clouds, you wouldn't have big clouds. And that's when it becomes fun - you don't have to spend your time thinking about what's happening - you just let it happen.</p>
-					<p class="pl-1">The only thing worse than yellow snow is green snow. Remember how free clouds are. They just lay around in the sky all day long. These trees are so much fun. I get started on them and I have a hard time stopping. We don't have anything but happy trees here. We'll do another happy little painting. In life you need color. This is truly an almighty mountain. Let's do it again then, what the heck. This is your world.  </p>
-				</div>
-			</div>
+<!--		Div containg contact section content-->
+		<div>
+
 		</div>
-		</div>
-		<div class="container mb-4">
-			<div class = "row d-flex justify-content-between">
+<!--		Div containing footer of whole page-->
+		<div class="container">
 
-				<div class = "col-sm-6 col-lg-3">
-					<div class="thumbnail text-center">
-						<div>
-							<img src = "http://www.fillmurray.com/202/202" alt = "Generic placeholder thumbnail" class="rounded-circle mb-4 mx-auto figure-img w-90">
-						</div>
-						<figure class = " figure-caption border rounded bg-light">
-							<figcaption class="text-center">
-								Let's get crazy. Isn't it great to do something you can't fail at? Let's make a happy little mountain now. You don't have to be crazy to do this but it does help. If you don't like it - change it. It's your world.
-							</figcaption>
-						</figure>
-					</div>
-				</div>
-
-				<div class = "col-sm-6 col-lg-3">
-					<div class="thumbnail text-center">
-						<div>
-							<img src = "http://www.fillmurray.com/201/201" alt = "Generic placeholder thumbnail" class="rounded-circle mb-4 mx-auto figure-image w-90"/>
-						</div>
-						<figure class = " figure-caption border rounded bg-light">
-							<figcaption class="text-center">
-								That's a crooked tree. We'll send him to Washington. Just let this happen. We just let this flow right out of our minds. Just think about these things in your mind and drop em' on canvas. And just raise cain. Isn't it great.
-							</figcaption>
-						</figure>
-					</div>
-				</div>
-
-				<div class = "col-sm-6 col-lg-3">
-					<div class="thumbnail text-center">
-						<div>
-							<img src = "http://www.fillmurray.com/199/199" alt = "Generic placeholder thumbnail" class="rounded-circle mb-4 mx-auto figure-img w-90"/>
-						</div>
-						<figure class = " figure-caption border rounded bg-light">
-							<figcaption class="text-center">
-								These trees are so much fun. I get started on them and I have a hard time stopping. You can create beautiful things - but you have to see them in your mind first. We need dark in order to show light.
-							</figcaption>
-						</figure>
-					</div>
-				</div>
-
-				<div class = "col-sm-6 col-lg-3">
-					<div class="thumbnail text-center">
-						<div>
-							<img src = "http://www.fillmurray.com/200/200" alt = "Generic placeholder thumbnail" class="rounded-circle mb-4 mx-auto figure-img w-90"/>
-						</div>
-						<figure class = " figure-caption border rounded bg-light">
-							<figcaption class="text-center">
-								Just take out whatever you don't want. It'll change your entire perspective. Remember how free clouds are. They just lay around in the sky all day long. Be careful. You can always add more - but you can't take it away.
-							</figcaption>
-						</figure>
-					</div>
-				</div>
-
-			</div>
-		</div>
-		<div class="container-fluid text-muted bg-dark mt-5" style="min-height: 6rem">
-			<footer class="footer bg-dark fixed-bottom">
-				<p class="d-flex justify-content-center m-0 py-5">Use your imagination, let it go.  Anything. As long as you believe.
-				</p>
-			</footer>
 		</div>
 	</body>
 </html>
