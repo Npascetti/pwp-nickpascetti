50d3b560dc8da6550fe79a81afc6e2f159db5780
Nicklas Pascetti <Npascetti@gmail.com>
2017-11-20 08:56:08 -0700

    added navbar section with list items

--- public_html/index.php
+++ public_html/index.php
@@ -18,8 +18,34 @@
 <!--		Div containing landing area, nav, and background image-->
 		<div class="container-fluid" id="bgimage">
 			<nav class="navbar">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item">
+						<a class="nav-link" href="#">About Me</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Portfolio</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Services</a>
+					</li>
+				</ul>
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item">
+						<a class="nav-link" href="#">Contact Form</a>
+					</li>
+				</ul>
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item">
+						<a class="nav-link" href="#">Legal</a>
+					</li>
+				</ul>
 
 			</nav>
+			<div>
+			<p>Hello and</p>
+			<p>Welcome to My</p>
+			<p>Portfolio Site</p>
+			</div>
 		</div>
 <!--		Div for services section-->
 		<div class="container">
@@ -27,7 +53,48 @@
 		</div>
 <!--		Div containing services cards-->
 		<div>
+			<div class = "row d-flex justify-content-between">
 
+				<div class = "col-sm-6 col-lg-3">
+					<div class="thumbnail text-center">
+						<div>
+							<h5>Elegance in Design and Function</h5>
+							<h6>UI|UX</h6>
+						</div>
+						<figure class = " figure-caption border rounded bg-light">
+							<figcaption class="text-center">
+								Let's get crazy. Isn't it great to do something you can't fail at? Let's make a happy little mountain now. You don't have to be crazy to do this but it does help. If you don't like it - change it. It's your world.
+							</figcaption>
+						</figure>
+					</div>
+				</div>
+
+				<div class = "col-sm-6 col-lg-3">
+					<div class="thumbnail text-center">
+						<div>
+							<h5>Mobile Web App</h5>
+							<h6>Creation and Conversion</h6>
+						</div>
+						<figure class = " figure-caption border rounded bg-light">
+							<figcaption class="text-center">
+								That's a crooked tree. We'll send him to Washington. Just let this happen. We just let this flow right out of our minds. Just think about these things in your mind and drop em' on canvas. And just raise cain. Isn't it great.
+							</figcaption>
+						</figure>
+					</div>
+				</div>
+
+				<div class = "col-sm-6 col-lg-3">
+					<div class="thumbnail text-center">
+						<div>
+							<h5>Custom WordPress</h5>
+							<h6>CMS Solutions</h6>
+						</div>
+						<figure class = " figure-caption border rounded bg-light">
+							<figcaption class="text-center">
+								These trees are so much fun. I get started on them and I have a hard time stopping. You can create beautiful things - but you have to see them in your mind first. We need dark in order to show light.
+							</figcaption>
+						</figure>
+					</div>
 		</div>
 <!--		div containing footer for services section-->
 		<div>
