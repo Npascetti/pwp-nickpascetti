9044ffb35c8d3025909d6de1f4279a11e4591e49
Nicklas Pascetti <Npascetti@gmail.com>
2017-11-20 10:13:59 -0700

    added some form inputs to contact section

--- public_html/index.php
+++ public_html/index.php
@@ -121,10 +121,25 @@
 		</div>
 <!--		Div containg contact section content-->
 		<div>
-
+			<form>
+				<div class="form-group">
+					<label for="exampleInputEmail1">Email address</label>
+					<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+					<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+				</div>
+				<div class="form-group">
+					<label for="exampleInputPassword1">Password</label>
+					<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+				</div>
+				<div class="form-group">
+					<label for="exampleTextarea">Example textarea</label>
+					<textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
+				</div>
+				<button type="submit" class="btn btn-primary">Submit</button>
+			</form>
 		</div>
 <!--		Div containing footer of whole page-->
-		<footer class="page-footer" id="bgimage1">
+		<footer class="page-footer container-fluid" id="bgimage1">
 			<div class="container">
 				<div class="footer-copyright">
 					<div class="container-fluid">
