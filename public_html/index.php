0655aa8f78fb9139e607f8f45100c96d06bb7afc
Nicklas Pascetti <Npascetti@gmail.com>
2017-11-20 10:02:45 -0700

    made content headers and footers text right aligned

--- public_html/index.php
+++ public_html/index.php
@@ -14,10 +14,11 @@
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 		<title>Nick's PWP</title>
 	</head>
-	<body>
+	<div>
 <!--		Div containing landing area, nav, and background image-->
 		<div class="container-fluid" id="bgimage">
 			<nav class="navbar">
+				<a class="nav-link mr-auto" href="#">Logo Button</a>
 				<ul class="navbar-nav ml-auto">
 					<li class="nav-item">
 						<a class="nav-link" href="#">About Me</a>
@@ -39,19 +40,21 @@
 						<a class="nav-link" href="#">Legal</a>
 					</li>
 				</ul>
-
 			</nav>
-			<div>
-			<p>Hello and</p>
-			<p>Welcome to My</p>
-			<p>Portfolio Site</p>
+			<div class="text-left">
+			<h2>Hello and</h2>
+			</div>
+			<div class="text-center">
+				<h2>Welcome To My</h2>
+			</div>
+			<div class="text-right">
+				<h2>Portfolio Site</h2>
+			</div>
 			</div>
 		</div>
 <!--		Div for services section-->
 		<div class="container border">
-			<div class="row">
-				<h2>Services</h2>
-			</div>
+				<h2 class="text-right">Services</h2>
 		</div>
 <!--		Div containing services cards-->
 		<div class="container">
@@ -101,19 +104,19 @@
 <!--		div containing footer for services section-->
 		<div class="container border">
 			<h3>Always Mobile First</h3>
-			<h3 class="ml-auto">Always Responsive Design</h3>
+			<h3 class="text-right">Always Responsive Design</h3>
 		</div>
 <!--		Div containing the portfolio section-->
 		<div class="container">
 			<div class="container border">
-				<h1>Portfolio</h1>
+				<h1 class="text-right">Portfolio</h1>
 			</div>
 
 		</div>
 <!--		Div containg header for Contact Section-->
 		<div class="container border">
 			<div class="container border">
-				<h1>Contact</h1>
+				<h1 class="text-right">Contact</h1>
 			</div>
 		</div>
 <!--		Div containg contact section content-->
