5008bd7b3cb111ae363ec4e1e5b2499f31ca9daa
Nicklas Pascetti <Npascetti@gmail.com>
2017-11-26 23:22:05 -0700

    js linked and jquery tested and working

--- public_html/index.php
+++ public_html/index.php
@@ -5,9 +5,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 		<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" />
 		<link rel="stylesheet" href="css/stylesheet.css" type="text/css" />
-		<script src="JS/animations.js" type="text/javascript" async></script>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-		<script src="//cdn.jsdelivr.net/velocity/1.1.0/velocity.min.js"></script>
+		<script src="js/animations.js" type="text/javascript" async></script>
 
 		<!--Bootstrap CSS-->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous" />
@@ -15,6 +13,7 @@
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/velocity/1.5.0/velocity.min.js"></script>
 		<title>Nick's PWP</title>
 	</head>
 <!--		Div containing landing area, nav, and background image-->
@@ -58,7 +57,7 @@
 <!--		Div for services section-->
 	<div class="services">
 		<div class="container-fluid rounded py-3 secheader">
-				<h2 class="text-right"><strong>S</strong>ervices</h2>
+				<h1 class="text-right"><strong>S</strong>ervices</h1>
 		</div>
 <!--		Div containing services cards-->
 		<div class="dividerbg">
